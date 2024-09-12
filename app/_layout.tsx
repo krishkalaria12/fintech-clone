c9dfd0e2eaff23aa6c7383e5b13fbb68e62bd1fe
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-09-12 16:29:11 +0530

    feat: Implement the home screen and added the assest

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,4 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -14,11 +9,6 @@ export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -48,5 +38,9 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  return <Stack></Stack>;
+  return(
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
